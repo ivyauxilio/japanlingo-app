a204b9a4ee28448629ea6ec1b406b973874bb068
ivyauxilio <auxilioivy30@gmail.com>
2025-04-13 23:38:58 +0800

    update next config file

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   // webpack: (config) => {
   //   config.resolve.fallback = { fs: false, net: false, tls: false };
   //   return config;
